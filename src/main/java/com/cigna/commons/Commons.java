982a8a39f98f60585a290035548b0a9801417df1
Pradeep <mpradeep410@gmail.com>
2021-05-15 07:44:02 +0530

    TC001 testcase enhanced

--- src/main/java/com/cigna/commons/Commons.java
+++ src/main/java/com/cigna/commons/Commons.java
@@ -1,5 +1,8 @@
 package com.cigna.commons;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -7,6 +10,8 @@ import org.testng.Reporter;
 
 public class Commons 
 {
+	public FileInputStream fis;
+	public Properties prop;
 	
 	public void click(WebDriver driver,String xpathvalue,String text) throws Exception
 	{
@@ -20,6 +25,60 @@ public class Commons
 			System.out.println(e.getMessage());
 			Reporter.log(text + " is not clicked " + e.getMessage());
 		//	Assert.fail(text + " is not clicked");
+		}
+	}
+	
+	
+	public void navigateUrl(WebDriver driver,String url)
+	{
+		try
+		{
+			driver.get(url);
+			Reporter.log(url + " is entered");
+		}catch(Exception e)
+		{
+			
+			Reporter.log(url + " is not entered");
+			System.out.println("Navigate URL method is not working "+e.getMessage());
+			Assert.fail("URL is not entered");
+		}
+	}
+	
+	public void verifyTitle(WebDriver driver,String expectedTitle)
+	{
+		try
+		{
+			String actualTitle = driver.getTitle();
+			Assert.assertEquals(actualTitle, expectedTitle);
+			Reporter.log(actualTitle + " is matched with " + expectedTitle); 
+			
+		}catch(Exception e)
+		{
+			Reporter.log(expectedTitle + "Title is not matched");
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void currentURLValidation(WebDriver driver,String expectedURL) throws Exception
+	{
+		String actualURL = driver.getCurrentUrl();
+		Assert.assertEquals(actualURL, expectedURL);
+		Reporter.log(actualURL + " is matched with " + expectedURL); 		
+	}
+	
+	public Properties propertiesRead(String filePath)
+	{
+		try
+		{
+			 fis = new FileInputStream(filePath);
+			 prop = new Properties();
+			prop.load(fis);
+			
+			return prop;
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+			return null;
 		}
 	}
 
--- src/test/java/com/cigna/testcases/TC001.java
+++ src/test/java/com/cigna/testcases/TC001.java
@@ -1,20 +1,37 @@
 package com.cigna.testcases;
 
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import com.cigna.commons.Commons;
 import com.cigna.commons.OpenBrowser;
 
-public class TC001
+public class TC001 extends Commons
 {
 	OpenBrowser openBrowser = new OpenBrowser();
 	WebDriver driver;
 	
+	@BeforeSuite
+	public void configuration()
+	{
+		prop=propertiesRead(System.getProperty("user.dir") + "/src/test/resources/properties/config.properties");
+	}
+	
+	
+	
 	@Test
-	public void tc001()
+	public void tc001() throws Exception
 	{
 		driver = openBrowser.browserOpen(driver, "chrome");
-		driver.get("http://www.cigna.com");
+		System.out.println(prop.getProperty("Cigna_Prod_URL"));
+		navigateUrl(driver, prop.getProperty("Cigna_Prod_URL"));
+		currentURLValidation(driver, prop.getProperty("Cigna_Prod_URL"));
+		verifyTitle(driver, prop.getProperty("Cigna_Prod_Title"));
+		
+		//driver.get(prop.getProperty("Cigna_Prod_URL"));
 	}
 
 }
